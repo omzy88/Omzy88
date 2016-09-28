1a2ba082d14b7817c48d9c0e0db0037d3929ce0c
omzy88 <omhroszymarakhs@gmail.com>
2016-09-28 20:55:06 +0300

    Add files via upload

---
+++ python day 2/ex3.py
@@ -0,0 +1,16 @@
+num = input("Enter 10 digit number: ")
+
+odd = num[0]+" "+num[2]+" "+num[4]+" "+num[6]+" "+num[8]
+even = num[1]+" "+num[3]+" "+num[5]+" "+num[7]+" "+num[9]
+
+
+print(odd)
+print(end=" ")
+print(even)
+
+    
+    
+        
+
+
+                  
